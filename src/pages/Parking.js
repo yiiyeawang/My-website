7b104327dde08efcdd9d35e0d343a3f17fde2f55
yiiyeawang <yiiyeawang@gmail.com>
2022-10-03 16:47:13 +0800

    完成定位

--- src/pages/Parking.js
+++ src/pages/Parking.js
@@ -1,46 +1,59 @@
-import React from 'react'
+import React ,{ useState } from 'react'
 import Search from 'antd/lib/transfer/search';
 
 
-function geoFindMe() {
 
-    const status = document.querySelector('#status');
-    const mapLink = document.querySelector('#map-link');
-  
-    mapLink.href = '';
-    mapLink.textContent = '';
-  
-    function success(position) {
-      const latitude  = position.coords.latitude;
-      const longitude = position.coords.longitude;
-  
-      status.textContent = '';
-      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-    }
-  
-    function error() {
-      status.textContent = 'Unable to retrieve your location';
-    }
-  
-    if (!navigator.geolocation) {
-      status.textContent = 'Geolocation is not supported by your browser';
-    } else {
-      status.textContent = 'Locating…';
-      navigator.geolocation.getCurrentPosition(success, error);
-    }
-  
-  }
+
 
 
 
 
 
 function Parking() {
+    const { href,sethref}  = useState("")
+    const { textContent,setextContent}  = useState("")
+
+
+    function geoFindMe(e) {
+        e?.preventDefault();
+        //const status = document.querySelector('#status');
+        //const mapLink = document.querySelector('#map-link');
+        //console.log(status ,mapLink )
+        
+
+        function success(position) {
+          const latitude  = position.coords.latitude;
+          const longitude = position.coords.longitude;
+      
+          //status.textContent = '';
+
+          //mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+          //mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+          sethref(`https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`)
+          setextContent(`Latitude: ${latitude} °, Longitude: ${longitude} °`)
+        }
+      
+        function error() {
+        setextContent('Unable to retrieve your location')
+          //status.textContent = 'Unable to retrieve your location';
+        }
+      
+        if (!navigator.geolocation) {
+            setextContent('Geolocation is not supported by your browser')
+          //status.textContent = 'Geolocation is not supported by your browser';
+        } else {
+          //status.textContent = 'Locating…';
+          navigator.geolocation.getCurrentPosition(success, error);
+        }
+      
+      }
+
+
   return (
     <div style={{minHeight:"100vh"}}>
-        <p><button onclick={geoFindMe()}>Show my location</button></p>
-        <div id="out"></div>
+        <p><button onClick={geoFindMe()}>Show my location</button></p>
+        <div id="status" href={href} textContent={textContent}></div>
+        <a id='map-link' target="_blank"></a>
     </div>
   )
 }
